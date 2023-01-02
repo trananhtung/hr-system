41ba6cccc271f97ed8f76d9cdc10d0ba86c3cf28
tungtran <tunganhtran94@gmail.com>
2023-01-02 22:29:07 +0700

    add delelte API

--- employee_api/handlers/delete.go
+++ employee_api/handlers/delete.go
@@ -1,12 +1,31 @@
 package handlers
 
 import (
+	"HR-system/employee_api/models"
+	"HR-system/employee_api/responses"
 	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
 )
 
 func (s *Server) Delete(w http.ResponseWriter, r *http.Request) {
-	w.Header()
-	id := r.URL.Path[len("/api/v1/employee/"):]
-	w.Write([]byte("Delete account for id: " + id))
+	vars := mux.Vars(r)
+	idQuery := vars["id"]
+	id, err := strconv.Atoi(idQuery)
+	responses.CheckError(w, err != nil, nil)
+
+	tx := s.DB.Delete(&models.Employee{}, id)
+	if tx.Error != nil {
+		messages := []string{tx.Error.Error()}
+		responses.Error(w, http.StatusBadRequest, messages)
+		return
+	}
+
 	w.WriteHeader(http.StatusOK)
+	responses.Success(w, http.StatusOK, struct {
+		ID int `json:"id"`
+	}{
+		ID: id,
+	})
 }
